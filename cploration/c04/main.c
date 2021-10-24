6ac119b2ac1bba66c18dc28b5f859c35bd4d6030
himanshurd <91275023+himanshurd@users.noreply.github.com>
2021-10-23 19:52:45 -0700

    Update main.c

--- cploration/c04/main.c
+++ cploration/c04/main.c
@@ -1,41 +1,23 @@
+#include "parser.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#define MAX_LINE_LENGTH 200
+#include <stdlib.h> 
 
-/** Exercise 1 **/
 int main(int argc, const char *argv[]) {
-	/** Exersice 2 **/
-	if (argc != 2) {
-		printf("Usage: %s [filename]\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
 
-	/** Exercise 3 **/
-	FILE *fin;
-	fin = fopen(argv[1], "r");
-	if (fin == NULL) {
-		perror("Unable to open file!");
-		exit(EXIT_FAILURE);
-	}
+  if (argc < 2) {
+      printf("Usage: %s [filename]\n", argv[0]);
+      exit(EXIT_FAILURE);
+  }
 
-	/** Exercise 4 **/
-	char out_file[MAX_LINE_LENGTH];
-	strcpy(out_file, argv[1]);
-	strcat(out_file, ".echo");
-	FILE *fout = fopen(out_file, "w+");
+  FILE *fin = fopen(argv[1], "r");
+   
+  if (fin == NULL) {
+      perror("Unable to open file!");
+      exit(EXIT_FAILURE);
+   }
 
-	/** Exercise 5 **/
-	char line[MAX_LINE_LENGTH] = {0};
-	unsigned int line_num = 0;
-	while (fgets(line, sizeof(line), fin)) {
-		line_num++;
-		printf("[%04d] %s", line_num, line);
-		fprintf(fout,"%s", line);
-	}
-
-	/** Exercise 6 **/
-	fclose(fin);
-	fclose(fout);
-	return 0;
+  parse(fin);
+  fclose(fin);
+  
+  return 0;
 }
