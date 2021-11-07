74e8acf88296dde93e2a7d73b0d786fcb2222e12
himanshurd <91275023+himanshurd@users.noreply.github.com>
2021-11-06 22:35:11 -0700

    Update parser.h

--- cploration/c06/parser.h
+++ cploration/c06/parser.h
@@ -3,8 +3,35 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdbool.h>
-
+#include <stdint.h>
 #define MAX_LINE_LENGTH 200
+#define MAX_LABEL_LENGTH MAX_LINE_LENGTH - 2
+
+
+typedef int16_t hack_addr;
+typedef int16_t opcode;
+
+enum instr_type{
+  invalid  = -1,
+  A_TYPE,
+  C_TYPE,
+};
+
+typedef struct c_instruction {
+  opcode a:1;
+  opcode comp:6;
+  opcode dest:3;
+  opcode jump:3;
+} c_instruction;
+
+typedef struct a_instruction {
+  union {
+    hack_addr address;
+    char *label;
+  };
+
+  bool is_addr;
+} a_instruction;
 
 char *strip(char *s);
 
@@ -15,3 +42,5 @@ bool is_Atype(const char*);
 bool is_label(const char*);
 
 bool is_Ctype(const char*);
+
+char *extract_label(const char *line, char* label);
