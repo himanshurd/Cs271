dd0ad15064e747bd5a659989600fb0c07b052fc8
himanshurd <91275023+himanshurd@users.noreply.github.com>
2021-11-14 19:53:15 -0800

    Update main.c

--- cploration/c07/main.c
+++ cploration/c07/main.c
@@ -1,23 +1,60 @@
-#include "parser.h"
+#include "symtable.h"
 #include <stdio.h>
-#include <stdlib.h> 
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-int main(int argc, const char *argv[]) {
 
-  if (argc < 2) {
-      printf("Usage: %s [filename]\n", argv[0]);
-      exit(EXIT_FAILURE);
-  }
+int main()
+{        
+    // Create empty item
+    struct Symbol* item = (struct Symbol*) malloc(sizeof(struct Symbol));
+    item->name = NULL;  
+    item->address = -1; 
 
-  FILE *fin = fopen(argv[1], "r");
-   
-  if (fin == NULL) {
-      perror("Unable to open file!");
-      exit(EXIT_FAILURE);
-   }
+    // Labels from Max.asm
+    insert("OUTPUT_FIRST", 11);
+    printf("Inserting OUTPUT_FIRST at address 11\n");
+    insert("OUTPUT_D", 13);
+    printf("Inserting OUTPUT_D at address 13\n");
+    insert("INFINITE_LOOP", 15);
+    printf("Inserting INFINITE_LOOP at address 15\n");
+    printf("\n");
 
-  parse(fin);
-  fclose(fin);
-  
-  return 0;
+    // Display table
+    printf("Displaying table:\n");
+    display_table();
+    printf("\n");
+      
+    // Search label (FOUND)
+    char * label = "OUTPUT_FIRST";
+    printf("Looking up %s\n", label);
+    item = find(label);
+
+    if(item != NULL) {
+      printf("Element %s found: %d\n", item->name, item->address);
+    } else {
+      printf("Element %s not found\n", label);
+    }
+    printf("\n");
+
+    // Search label (NOT FOUND)
+    label = "MY_LABEL";
+    printf("Looking up %s\n", label);
+    item = find(label);
+
+    if(item != NULL) {
+      printf("Element %s found: %d\n", item->name, item->address);
+    } else {
+      printf("Element %s not found\n", label);
+    }
+    printf("\n");
+
+    // Display table again
+    printf("Displaying table:\n");
+    display_table();
+    printf("\n");
+
+    // Free memory
+    free(item);   
 }
