af546d5b503a01cbdbe52df5d438471cacbdce6a
himanshurd <91275023+himanshurd@users.noreply.github.com>
2021-11-20 22:35:58 -0800

    Update and rename parser 2.c to parser.c

--- cploration/c08/parser.c
+++ cploration/c08/parser.c
@@ -1,4 +1,6 @@
 #include "parser.h"
+#include "error.h"
+#include "symtable.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
@@ -7,7 +9,13 @@
 void parse(FILE * file) {
   char line[MAX_LINE_LENGTH] = {0};
   
-   while (fgets(line, sizeof(line), file)) {
+  unsigned int line_num = 0;
+  unsigned int instr_num = 0; 
+  while (fgets(line, sizeof(line), file)) {
+    line_num = line_num + 1;
+    if (instr_num > MAX_INSTRUCTIONS) {
+      exit_program(EXIT_TOO_MANY_INSTRUCTIONS, MAX_INSTRUCTIONS + 1);
+    }
      strip(line);
      if (!*line) {
        continue;
@@ -18,15 +26,27 @@ void parse(FILE * file) {
      }  else if (is_Ctype(line)) {
        inst_type = 'C';
      }  else if (is_label(line)) {
+       if (isalpha(line[0])) {
+         exit_program(EXIT_INVALID_LABEL, line_num, line);
+       } else if (symtable_find(line) != NULL) {
+          exit_program(EXIT_SYMBOL_ALREADY_EXISTS, line_num, line);
+       }
+       symtable_insert(line, instr_num);
+       continue;
+
        inst_type = 'L';
+       char new_label[MAX_LABEL_LENGTH];
+       extract_label(line, new_label);
+       printf("%c  %s\n", inst_type, new_label);
+       continue;
      }     
      printf("%c  %s\n", inst_type, line);
-     
+     instr_num = instr_num + 1;
    }
 }
 
 char *strip(char *s) {
-  char s_new[strlen(s)];
+  char s_new[strlen(s) + 1];
   int i = 0;
   for (char *s2 = s; *s2; s2++) {
     if ((*s2 == '/') && (*(s2 + 1) == '/')) {
@@ -51,4 +71,9 @@ bool is_label(const char *line) {
 
 bool is_Ctype(const char *line) {
   return !(is_Atype(line) || is_label(line));
+}
+
+char *extract_label(const char *line, char* label) {
+  strncpy(label, line + 1, strlen(line) - 2);
+  return label;
 }
