e37def181690aeadd988fa54efc4eb011470c1e0
himanshurd <91275023+himanshurd@users.noreply.github.com>
2021-12-05 18:26:29 -0800

    include files, define max, and symbol_id

---
+++ cploration/c10/hack.h
@@ -0,0 +1,33 @@
+#ifndef __HACK_H__
+#define __HACK_H__
+
+#define NUM_PREDEFINED_SYMBOLS 23
+#include <stdint.h>
+#include <string.h>
+
+
+typedef enum symbol_id {
+    SYM_RO = 0,
+    SYM_R1, 
+    SYM_R2,
+    SYM_R3,
+    SYM_R4,
+    SYM_R5,
+    SYM_R6,
+    SYM_R7,
+    SYM_R8,
+    SYM_R9,
+    SYM_R10,
+    SYM_R11,
+    SYM_R12,
+    SYM_R13,
+    SYM_R14,
+    SYM_R15,
+    SYM_SP = 0,
+    SYM_LCL,
+    SYM_ARG,
+    SYM_THIS,
+    SYM_THAT,
+    SYM_SCREEN = 16384,
+    SYM_KBD = 24576
+} symbol_id;
