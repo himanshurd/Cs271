947c16c4e6aaccf5505f73c486badab58da7303f
himanshurd <91275023+himanshurd@users.noreply.github.com>
2021-12-05 18:19:41 -0800

    Add all the includes and define all the Max values

---
+++ cploration/c10/parser.h
@@ -0,0 +1,18 @@
+#ifndef __PARSER_H__
+#define __PARSER_H__
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include "hack.h"
+
+#define MAX_LINE_LENGTH 200
+#define MAX_LABEL_LENGTH (MAX_LINE_LENGTH - 2)
+#define MAX_HACK_ADDRESS INT16_MAX
+#define MAX_INSTRUCTIONS MAX_HACK_ADDRESS
+
+typedef int16_t hack_addr;
+typedef int16_t opcode;
